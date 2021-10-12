f06f7dab432463c166fcf6315c24246de530c174
Henry Jalonen <henry.jalonen@solita.fi>
2021-10-12 13:42:38 +0300

    Update Android Gradle Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltVersion}")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
