5b47662af471a1418235c2e3ef16017aec14fc70
Henry Jalonen <henry.jalonen@solita.fi>
2021-10-27 12:10:47 +0300

    Update verifiable-credentials dependency

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -10,6 +10,6 @@ java {
 
 dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    api("fi.dvv.digiid.poc.vc:digiid-poc-verifiable-credentials:0.0.3")
+    api("fi.dvv.digiid.poc.vc:digiid-poc-verifiable-credentials:0.0.6")
     api("com.squareup.okhttp3:okhttp-tls:4.9.1")
 }
