65541057efdd529fdd62b65908fea11f357c8296
Henry Jalonen <henry.jalonen@solita.fi>
2021-10-28 20:31:15 +0300

    Implement actual credential verification

--- app/src/main/java/fi/dvv/digiid/poc/wallet/di/ProfileModule.kt
+++ app/src/main/java/fi/dvv/digiid/poc/wallet/di/ProfileModule.kt
@@ -12,7 +12,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface RepositoryModule {
+interface ProfileModule {
     @Binds
     @Singleton
     fun bindProfileRepository(profileRepositoryImpl: ProfileRepositoryImpl): ProfileRepository
--- data/src/main/java/fi/dvv/digiid/poc/data/network/WalletService.kt
+++ data/src/main/java/fi/dvv/digiid/poc/data/network/WalletService.kt
@@ -2,8 +2,12 @@ package fi.dvv.digiid.poc.data.network
 
 import fi.dvv.digiid.poc.vc.VerifiablePresentation
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface WalletService {
     @GET("api/0.1/coreid")
     suspend fun getCoreID(): VerifiablePresentation
+
+    @GET
+    suspend fun getVerificationMethod(@Url url: String): String
 }
